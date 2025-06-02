47808f6342cc3c243ec2c15108f247d45f1765f8
l19ux <zeebraman43@gmail.com>
2025-06-02 09:53:43 +0530

    Create testData.js

---
+++ testData.js
@@ -0,0 +1,450 @@
+ const testData = [{
+  date: '2024-07-28',
+  my_marks: 79.5,
+  avg_marks: 77.97,
+  median_marks: 77,
+  topper_marks: 200,
+  my_rank: 241135,
+  total: 535378
+},{
+  date: '2024-08-23',
+  my_marks: 89.5,
+  avg_marks: 99.27,
+  median_marks: 94.5,
+  topper_marks: 200,
+  my_rank: 99568,
+  total: 158917
+},{
+  date: '2024-08-24',
+  my_marks: 97,
+  avg_marks: 123.53,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 123313,
+  total: 154523
+},{
+  date: '2024-08-26',
+  my_marks: 109,
+  avg_marks: 116.72,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 89461,
+  total: 141070
+},{
+  date: '2024-08-26',
+  my_marks: 91,
+  avg_marks: 109.28,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 98110,
+  total: 132016
+},{
+  date: '2024-08-27',
+  my_marks: 108.5,
+  avg_marks: 110.83,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 67322,
+  total: 119693
+},{
+  date: '2024-08-29',
+  my_marks: 115,
+  avg_marks: 118.1,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 65839,
+  total: 113691
+},{
+  date: '2024-08-30',
+  my_marks: 120,
+  avg_marks: 119.08,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 59416,
+  total: 110206
+},{
+  date: '2024-08-31',
+  my_marks: 105.5,
+  avg_marks: 115.48,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 66548,
+  total: 101118
+},{
+  date: '2024-09-02',
+  my_marks: 119.55,
+  avg_marks: 112.5,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 60716,
+  total: 95768
+},{
+  date: '2024-09-03',
+  my_marks: 116.5,
+  avg_marks: 118.22,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 51815,
+  total: 91564
+},{
+  date: '2024-09-06',
+  my_marks: 108,
+  avg_marks: 102.12,
+  median_marks:110 ,
+  topper_marks: 200,
+  my_rank: 79600,
+  total: 178714
+},{
+  date: '2024-09-16',
+  my_marks: 138.5,
+  avg_marks: 105.92,
+  median_marks: 115.5,
+  topper_marks: 200,
+  my_rank: 50875,
+  total: 223488
+},{
+  date: '2024-09-17',
+  my_marks: 128.5,
+  avg_marks: 96.13,
+  median_marks: 94.5,
+  topper_marks: 200,
+  my_rank: 30382,
+  total: 147459
+},{
+  date: '2024-09-18',
+  my_marks: 119,
+  avg_marks: 96.14,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 35065,
+  total: 116557
+},{
+  date: '2024-09-20',
+  my_marks: 100,
+  avg_marks: 86.14,
+  median_marks: 94.5,
+  topper_marks: 200,
+  my_rank: 34392,
+  total: 95111
+},{
+  date: '2024-09-20',
+  my_marks: 113,
+  avg_marks: 119.89,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 52519,
+  total: 87177
+},{
+  date: '2024-09-23',
+  my_marks: 123.5,
+  avg_marks: 114.92,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 35229,
+  total: 81284
+},{
+  date: '2024-09-24',
+  my_marks: 137,
+  avg_marks: 121.86,
+  median_marks: 130 ,
+  topper_marks: 200,
+  my_rank: 27984,
+  total:75948
+},{
+  date: '2024-09-25',
+  my_marks: 111,
+  avg_marks: 109.11,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 36177,
+  total: 70179
+},{
+  date: '2024-09-30',
+  my_marks: 99.5,
+  avg_marks: 107.86,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 42552,
+  total: 69258
+},{
+  date: '2024-10-03',
+  my_marks: 127,
+  avg_marks: 119.26,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 33486,
+  total: 71663
+},{
+  date: '2024-10-04',
+  my_marks: 119.5,
+  avg_marks: 123.11,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 41992,
+  total: 69931
+},{
+  date: '2024-10-06',
+  my_marks: 129.5,
+  avg_marks: 116.23,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 24522,
+  total: 64035
+},{
+  date: '2024-10-09',
+  my_marks: 115.5,
+  avg_marks: 117.21,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 37125,
+  total: 65849
+},{
+  date: '2024-10-11',
+  my_marks: 104,
+  avg_marks: 113.98,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 42467,
+  total: 65500
+},{
+  date: '2024-10-13',
+  my_marks: 114.5,
+  avg_marks: 109.93,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 32901,
+  total: 67652
+},{
+  date: '2024-10-15',
+  my_marks: 115,
+  avg_marks: 114.04,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 36748,
+  total: 73919
+},{
+  date: '2024-10-16',
+  my_marks: 115.5,
+  avg_marks: 113.04,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 36988,
+  total: 70877
+},{
+  date: '2024-10-18',
+  my_marks: 128.5,
+  avg_marks: 110.91,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 26042,
+  total: 85612
+},{
+  date: '2024-10-20',
+  my_marks: 110.5,
+  avg_marks: 112.81,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 44665,
+  total: 77654
+},{
+  date: '2024-10-21',
+  my_marks: 128.5,
+  avg_marks: 110.92,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 26042,
+  total: 85613
+},{
+  date: '2024-10-23',
+  my_marks: 134,
+  avg_marks: 125.07,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 39306,
+  total: 88931
+},{
+  date: '2024-10-28',
+  my_marks: 121.5,
+  avg_marks: 123.83,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 50604,
+  total: 87090
+},{
+  date: '2024-10-30',
+  my_marks: 112.5,
+  avg_marks: 99,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 69330,
+  total: 192550
+},{
+  date: '2024-11-01',
+  my_marks: 132,
+  avg_marks: 116.61,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 78797,
+  total: 215025
+},{
+  date: '2024-11-04',
+  my_marks: 115.5,
+  avg_marks: 104.12,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 92181,
+  total: 235946
+},{
+  date: '2024-11-07',
+  my_marks: 154,
+  avg_marks: 115.71,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 16025,
+  total: 264020
+},{
+  date: '2024-11-09',
+  my_marks: 115.5,
+  avg_marks: 101.16,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 102618,
+  total: 298692
+},{
+  date: '2024-11-14',
+  my_marks: 130,
+  avg_marks: 93.3,
+  median_marks: 94.5,
+  topper_marks: 200,
+  my_rank: 40390,
+  total: 366501
+},{
+  date: '2025-05-26',
+  my_marks: 136,
+  avg_marks: 104.51,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 39978,
+  total: 145007
+},{
+  date: '2025-05-26',
+  my_marks: 119,
+  avg_marks: 99.45,
+  median_marks: 94.5,
+  topper_marks: 200,
+  my_rank: 30585,
+  total: 90261
+},{
+  date: '2025-05-27',
+  my_marks: 123,
+  avg_marks: 111.2,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 31541,
+  total: 71712
+},{
+  date: '2025-05-27',
+  my_marks: 131.5,
+  avg_marks: 110.36,
+  median_marks: 115.5,
+  topper_marks: 200,
+  my_rank: 16979,
+  total: 59557
+},{
+  date: '2025-05-27',
+  my_marks: 108.5,
+  avg_marks: 92.18,
+  median_marks: 90,
+  topper_marks: 200,
+  my_rank: 16817,
+  total: 48206
+},{
+  date: '2025-05-28',
+  my_marks: 119.5,
+  avg_marks: 128.5,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 27300,
+  total: 38481
+},{
+  date: '2025-05-28',
+  my_marks: 126.5,
+  avg_marks: 117.89,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 16245,
+  total: 34075
+},{
+  date: '2025-05-29',
+  my_marks: 123.5,
+  avg_marks: 123.01,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 17859,
+  total: 30602
+},{
+  date: '2025-05-29',
+  my_marks: 122,
+  avg_marks: 122.77,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 17507,
+  total: 28681
+},{
+  date: '2025-05-29',
+  my_marks: 111.5,
+  avg_marks: 123.46,
+  median_marks: 123.5,
+  topper_marks: 200,
+  my_rank: 18086,
+  total: 25135
+},{
+  date: '2025-05-30',
+  my_marks: 96,
+  avg_marks: 114.43,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 17514,
+  total: 22609
+},{
+  date: '2025-05-30',
+  my_marks: 128,
+  avg_marks: 120.38,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 9928,
+  total: 20615
+},{
+  date: '2025-05-31',
+  my_marks: 141.5,
+  avg_marks: 123.38,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 6105,
+  total: 18683
+},{
+  date: '2025-05-31',
+  my_marks: 119,
+  avg_marks: 109.38,
+  median_marks: 130,
+  topper_marks: 200,
+  my_rank: 7922,
+  total: 17124
+},{
+  date: '2025-06-01',
+  my_marks: 105,
+  avg_marks: 101.15,
+  median_marks: 110,
+  topper_marks: 200,
+  my_rank: 7744,
+  total: 14819
+},{
+  date: '2025-06-01',
+  my_marks: 122.5,
+  avg_marks: 112.06,
+  median_marks: 123.5,
+  topper_marks: 200,
+  my_rank: 6291,
+  total: 14139
+}
+/* your large array as above - unchanged, omitted here for brevity */];
