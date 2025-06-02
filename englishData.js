81d41b94033fa367f0fd3a28c199941b0e898f0c
l19ux <zeebraman43@gmail.com>
2025-06-02 10:02:32 +0530

    Create englishData.js

---
+++ englishData.js
@@ -0,0 +1,6 @@
+    const labelsEnglish = [
+      "Spellings", "Synonyms", "Antonyms", "One word substitute", "Idioms",
+      "Error Spotting", "Vocab", "Grammar", "Active→Passive", "Passive→Active",
+      "Direct→Indirect", "Indirect→Direct", "Sent. Improve", "Cloze Test"
+    ];
+    const englishData = [86, 64, 63, 82, 89, 61, 71, 74, 54, 19, 30, 43, 85, 94];
