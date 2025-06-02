8894a8db2e4e7c33bc40eccc12891ddfa8392a17
l19ux <zeebraman43@gmail.com>
2025-06-02 10:03:40 +0530

    Create mathsData.js

---
+++ mathsData.js
@@ -0,0 +1,20 @@
+    const mathsLabels = [
+      "Number System", "Basic Algebra Identities", "Algebra Linear Equation", "Algebra", "Percentage",
+      "Divisibility and Remainder", "Ratio and Proportions", "Compound Interest", "Simple Interest",
+      "Profit & Loss", "Average", "Graph", "Discount & Marked Price", "Partnership Business",
+      "Successive Selling", "Mixture and Alligation", "Time and Distance", "Time and Work",
+      "Trigonometry - Height and Distance", "Trigonometry", "Triangle", "Trigonometric Ratios & Identities",
+      "Circles", "Geometry", "Histogram and Frequency", "Mensuration 2D", "Mensuration 2D - Triangles",
+      "Mensuration 3D Solid Figure 1", "Mensuration 3D Solid Figure 2", "Simplification", "Fraction",
+      "BODMAS -Rule", "Square Roots & Elementary Surds", "Bar-Graph", "Pie-chart", "Installments"
+    ];
+    const mathsData = [
+      79.41, 93.65, 100, 88.27, 61.13,
+      94.5, 69.36, 74.58, 77.2,
+      65.3, 39.94, 71.76, 59.54, 62.05,
+      78.7, 50.8, 72.87, 45.35,
+      88.93, 91.94, 85.13, 82.04,
+      60.32, 81.63, 73.74, 100, 49,
+      76.67, 71.73, 88.87, 80.68,
+      96.97, 83.54, 70.28, 63.23, 35.05
+    ];
