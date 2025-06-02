df3b83b671c33d6116e9abc9856f4b7a2a3e525f
l19ux <zeebraman43@gmail.com>
2025-06-02 10:03:08 +0530

    Create reasoningData.js

---
+++ reasoningData.js
@@ -0,0 +1,17 @@
+    const reasoningLabels = [
+      "Semantic Analogy", "Symbolic/Number Analogy", "Figural Analogy", "Semantic Classification",
+      "Symbolic/Number Classification", "Figural Classification", "Coding Decoding - Letter Shifting",
+      "Coding Decoding - Operations on Place Value", "Coding Decoding", "Ven Diagram", "Word Building",
+      "Numerical and symbolic operations", "Punched Hole/ Pattern-Folding & Un-folding", "Figural Pattern - completion",
+      "Cube and Dice - 1", "Cube and Dice - 2", "Mirror Image", "Counting Figures", "Embedded Figures",
+      "Semantic Series", "Number Series", "Figural Series", "Problem Solving - Quant based Word Problems",
+      "Blood Relation", "Missing Number", "Direction and Distance", "Index, Address, Date and City Matching",
+      "Similarities and Differences", "Critical Thinking and Drawing Inferences", "Seating Arrangement",
+      "Clock and Calendar", "Ordering and Ranking", "Decision Making, emotional and social intelligence",
+      "Syllogistic Reasoning", "Statement & Conclusion"
+    ];
+    const reasoningScores = [
+      87.26, 65.17, 100, 91.34, 40.85, 82.37, 76.71, 84.74, 60.09, 86.98, 100,
+      87.29, 100, 60.84, 43.47, 39.33, 100, 44.05, 100, 100, 96.65, 100, 68.89,
+      82.05, 50.71, 79.93, 82.9, 88.78, 71.15, 57.75, 37.78, 46.68, 37.48, 100, 91.79
+    ];
